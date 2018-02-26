236bb64166424d7f7041366e05f3c59737c9ef71
Tony Chau <tony0chau@gmail.com>
2018-02-26 21:41:23 +0100

    File check update
    Will check music folder for location, compare with playlist, and delete
    files not on playlist

--- youtubePlaylistToMP3_downloader.py
+++ youtubePlaylistToMP3_downloader.py
@@ -1,17 +1,17 @@
 import os
 import json
-import shutil
 import youtube_dl
 
 
 class YoutubePlaylistToMp3Downlaoder:
     def __init__(self, music_folder, playlist_url):
+        self.playlist_info_filename = 'playlist_info.json'
+
         self.music_folder = music_folder
         self.playlist_url = playlist_url
-        self.playlist_info_json_filename = '{}/playlist_info.json'.format(music_folder)
-        self.download_check_json_filename = '{}/download_check.json'.format(music_folder)
+        self.playlist_info_json_filename = '{}/{}'.format(music_folder, self.playlist_info_filename)
 
-        self.download_url_list = []
+        self.download_url_map = {}
 
     def create_playlist_info_json_file(self):
         print('Creating JSON-file...')
@@ -25,21 +25,29 @@ class YoutubePlaylistToMp3Downlaoder:
         with open(filename) as json_data:
             parsed_json = json.load(json_data)
 
-        video_url_list = []
+        video_url_list = {}
         for video_info in parsed_json['entries']:
-            video_url_list.append(video_info['url'])
+            video_id = video_info['id']
+            title = video_info['title']
+            if video_id not in video_url_list:
+                video_url_list[video_id] = title
 
         return video_url_list
 
-    def filter_list(self, full_list, excludes):
-        return [x for x in full_list if x not in excludes]
+    def filter_map(self, full_map, exclude_map):
+        new_map = {}
+        for key, value in full_map.items():
+            if key not in exclude_map:
+                new_map[key] = value
+
+        return new_map
 
     def download_mp3_from_url_list(self):
-        print('Videos to download: {}'.format(len(self.download_url_list)))
+        print('Videos to download: {}'.format(len(self.download_url_map)))
         print('Download Start...')
 
         ydl_opts = {
-            'outtmpl': 'music/%(title)s.%(ext)s',
+            'outtmpl': 'music/%(id)s#%(title)s.%(ext)s',
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
@@ -49,7 +57,7 @@ class YoutubePlaylistToMp3Downlaoder:
         }
 
         youtube_video_links = []
-        for url in self.download_url_list:
+        for url in self.download_url_map:
             link = 'https://www.youtube.com/watch?v={}'.format(url)
             youtube_video_links.append(link)
 
@@ -58,6 +66,37 @@ class YoutubePlaylistToMp3Downlaoder:
 
         print('Download finished...')
 
+    def get_predownloaded_files_map(self):
+        file_list = []
+        for (dirpath, dirnames, filenames) in os.walk(self.music_folder):
+            file_list.extend(filenames)
+            break
+
+        # Filter filename that ends with <.mp3>
+        file_list = [file for file in file_list if file.endswith(".mp3")]
+
+        predownloaded_files_map = {}
+        for filename in file_list:
+            (key, value) = filename.split('#', 1)
+
+            if(key not in predownloaded_files_map):
+                # Remove mp3 from value
+                predownloaded_files_map[key] = value[:len(value) - len('.mp3')]
+
+        return predownloaded_files_map
+
+    def delete_files_not_in_playlist(self, file_map, playlist_map):
+        copy_map = self.filter_map(file_map, playlist_map)
+        print()
+        for key, value in copy_map.items():
+            filename = '{}#{}.mp3'.format(key, value)
+            # Remove file
+            os.remove('{}/{}'.format(self.music_folder, filename))
+            # Remove from map
+            del file_map[key]
+            print('Removed file: {}'.format(filename))
+        print()
+
     def start_download(self):
         # Create
         if not os.path.exists(self.music_folder):
@@ -65,17 +104,13 @@ class YoutubePlaylistToMp3Downlaoder:
 
         # Create JSON-file with playlist info
         self.create_playlist_info_json_file()
-
         video_url_list = self.get_url_list_from_json_file(self.playlist_info_json_filename)
 
+        predownloaded_files_map = self.get_predownloaded_files_map()
+        self.delete_files_not_in_playlist(predownloaded_files_map, video_url_list)
+
         # Excludes videos downloaded on earlier run
-        video_url_check_list = []
-        if os.path.isfile(self.download_check_json_filename):
-            video_url_check_list = self.get_url_list_from_json_file(self.download_check_json_filename)
-        self.download_url_list = self.filter_list(video_url_list, video_url_check_list)
+        self.download_url_map = self.filter_map(video_url_list, predownloaded_files_map)
 
         # Download list
         self.download_mp3_from_url_list()
-
-        # Update exclude list
-        shutil.copy(self.playlist_info_json_filename, self.download_check_json_filename)
